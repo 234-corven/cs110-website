48ceae6d92a57c41ad8ebd0c5591095db34072d4
Corven Neal <cneal005@ucr.edu>
2025-07-10 01:58:15 +0900

    most of the components done

--- src/components/Feed.vue
+++ src/components/Feed.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="posts"> 
+  <div class="postBox"> 
     Posts
   </div>
 </template>
@@ -9,5 +9,17 @@
 </script>
 
 <style>
+
+.postBox {
+  position: fixed;
+  top: 15%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 750px;
+  padding: 20px;
+  background-color: rgb(236, 233, 28);
+  border-radius: 8px;
+  box-shadow: 0 5px 15px rgba(0,0,0,0.1);
+}
 
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,33 +3,22 @@ import { RouterLink, RouterView } from 'vue-router';
 import Navigation from '../components/Navigation.vue';
 import Login from '../components/Login.vue';
 import WhoToFollow from '../components/whoToFollow.vue';
+import Feed from '../components/Feed.vue';
 
 import { ref } from 'vue';
 </script>
 
 <template>
-  <header>
-      <div class ="navBar">
-        <nav>
-          <Navigation/>
-        </nav>
-      </div> 
-  </header>
-
-  <RouterView/>
-  
+   
   <div class="frontLoginBox">
     <RouterLink to="/login">Log In</RouterLink>
   </div>
 
-  <div class="postBox">
+  <Feed/>
 
-  </div>
 
-  <div >
-    <whoToFollow/>
-  </div>
-
+  <whoToFollow/>
+  
   <RouterView/>
 
 
@@ -55,18 +44,6 @@ color:blueviolet;
   background-color: rgb(236, 233, 28);
   border-radius: 8px;
   box-shadow: 0 2px 10px rgba(0,0,0,0.1);
-}
-
-.postBox {
-  position: fixed;
-  top: 15%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  width: 750px;
-  padding: 20px;
-  background-color: rgb(236, 233, 28);
-  border-radius: 8px;
-  box-shadow: 0 5px 15px rgba(0,0,0,0.1);
 }
 
 
