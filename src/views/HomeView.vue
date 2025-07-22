3a8f8862bb0e0116f4b866c6f59abc948f4aa4cb
Corven Neal <cneal005@ucr.edu>
2025-07-22 20:32:30 +0900

    page layout changes

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -78,7 +78,7 @@ export default {
   flex: 0 0 300px;
   display: flex;
   flex-direction: column;
-  gap: 20px;
+  gap: 5px;
   margin-top: 50px;
 }
 
@@ -93,7 +93,7 @@ export default {
   flex: 0 0 300px;
   display: flex;
   flex-direction: column;
-  gap: 20px;
+  gap: 5px;
   margin-top: 50px;
 }
 
